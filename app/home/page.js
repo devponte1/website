4b4af138bbe2d2346640bbec7cdedaa6106fee5e
devponte1 <pontus.hultgren11@gmail.com>
2025-05-01 20:01:49 +0200

    added /home

---
+++ app/home/page.js
@@ -0,0 +1,11 @@
+// app/home/page.js
+export default function HomePage() {
+    return (
+      <div>
+        <title>Home</title>
+        <h1>Home</h1>
+        <p>welcome to my new website.</p>
+      </div>
+    )
+  }
+  
