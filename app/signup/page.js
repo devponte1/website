c39acfdae2bb154994eaeb66778c74c5ba363ca0
devponte1 <pontus.hultgren11@gmail.com>
2025-05-03 14:43:30 +0200

    commit

--- app/signup/page.js
+++ app/signup/page.js
@@ -14,7 +14,7 @@ export default function SignupPage() {
     e.preventDefault();
     setStatus('Creating account...');
 
-    const res = await fetch(`website.loca.lt/api/signup`, {
+    const res = await fetch(`https://website.loca.lt/api/signup`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ username, password }),
